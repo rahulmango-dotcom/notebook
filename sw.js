ef9574720ec2f7237d5e56ae48a10295356cc93e
rahulmango-dotcom <rahulnza@gmail.com>
2026-08-18 00:50:56 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,26 @@
+// sw.js – Service Worker for offline caching
+const CACHE_NAME = 'notebook-v1';
+const urlsToCache = [
+  './'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    clients.claim()
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
